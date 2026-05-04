e381be246c5303b72c195e41d6234d054114d67a
ouatu-ro <git@ouatu.ro>
2026-05-05 00:36:40 +0300

    Remove obsolete compatibility shim modules

--- mtm/meta_interpreter.py
+++
@@ -1,4 +0,0 @@
-"""Compatibility re-exports for the Meta-ASM host runtime."""
-
-from .meta_asm_host import *
-
--- mtm/outer_tape.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility re-exports for compiled-band and encoding helpers."""
-
-from .compiled_band import *
-from .tape_encoding import *
-
